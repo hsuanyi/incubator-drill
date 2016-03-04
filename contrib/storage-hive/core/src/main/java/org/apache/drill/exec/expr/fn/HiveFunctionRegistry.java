cf90240d71898ecdadfe51f583fe4e1271058e88
Hsuan-Yi Chu <hsuanyi@usc.edu>
2016-03-03 20:14:59 -0800

    DRILL-4372: (continued) Type inference for HiveUDFs

--- contrib/storage-hive/core/src/main/java/org/apache/drill/exec/expr/fn/HiveFunctionRegistry.java
+++ contrib/storage-hive/core/src/main/java/org/apache/drill/exec/expr/fn/HiveFunctionRegistry.java
@@ -19,17 +19,26 @@ package org.apache.drill.exec.expr.fn;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Collection;
 
+import org.apache.calcite.rel.type.RelDataType;
+import org.apache.calcite.rel.type.RelDataTypeFactory;
+import org.apache.calcite.sql.SqlOperatorBinding;
+import org.apache.calcite.sql.type.SqlReturnTypeInference;
+import org.apache.calcite.sql.type.SqlTypeName;
 import org.apache.drill.common.config.DrillConfig;
+import org.apache.drill.common.exceptions.UserException;
 import org.apache.drill.common.expression.FunctionCall;
 import org.apache.drill.common.scanner.ClassPathScanner;
 import org.apache.drill.common.scanner.persistence.ScanResult;
+import org.apache.drill.common.types.TypeProtos;
 import org.apache.drill.common.types.TypeProtos.MajorType;
 import org.apache.drill.common.types.TypeProtos.MinorType;
 import org.apache.drill.common.types.Types;
 import org.apache.drill.exec.expr.fn.impl.hive.ObjectInspectorHelper;
 import org.apache.drill.exec.planner.sql.DrillOperatorTable;
 import org.apache.drill.exec.planner.sql.HiveUDFOperator;
+import org.apache.drill.exec.planner.sql.TypeInferenceUtils;
 import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDF;
 import org.apache.hadoop.hive.ql.udf.UDFType;
@@ -70,7 +79,9 @@ public class HiveFunctionRegistry implements PluggableFunctionRegistry{
   @Override
   public void register(DrillOperatorTable operatorTable) {
     for (String name : Sets.union(methodsGenericUDF.asMap().keySet(), methodsUDF.asMap().keySet())) {
-      operatorTable.add(name, new HiveUDFOperator(name.toUpperCase()));
+      Collection<Class<? extends GenericUDF>> gUDFs = methodsGenericUDF.asMap().get(name);
+      Collection<Class<? extends UDF>> uDFs = methodsUDF.asMap().get(name);
+      operatorTable.add(name, new HiveUDFOperator(name.toUpperCase(), new HiveSqlReturnTypeInference(gUDFs, uDFs)));
     }
   }
 
@@ -204,4 +215,114 @@ public class HiveFunctionRegistry implements PluggableFunctionRegistry{
     return null;
   }
 
+  public class HiveSqlReturnTypeInference implements SqlReturnTypeInference {
+    private final Collection<Class<? extends GenericUDF>> gUDFs;
+    private final Collection<Class<? extends UDF>> uDFs;
+    private HiveSqlReturnTypeInference(Collection<Class<? extends GenericUDF>> gUDFs, Collection<Class<? extends UDF>> uDFs) {
+      this.gUDFs = gUDFs;
+      this.uDFs = uDFs;
+    }
+
+    @Override
+    public RelDataType inferReturnType(SqlOperatorBinding opBinding) {
+      final RelDataTypeFactory factory = opBinding.getTypeFactory();
+      final RelDataType anyType = factory.createTypeWithNullability(
+          factory.createSqlType(SqlTypeName.ANY),
+          true);
+
+      // The following logic is just a safe play:
+      // Even if any of the input arguments has ANY type,
+      // it "might" still be possible to determine the return type based on other non-ANY types
+      for (RelDataType type : opBinding.collectOperandTypes()) {
+        if (TypeInferenceUtils.getDrillTypeFromCalciteType(type) == TypeProtos.MinorType.LATE) {
+          return anyType;
+        }
+      }
+
+      final RelDataType returnDataType = getReturnType(opBinding, false);
+      if(!returnDataType.equals(anyType)) {
+        return returnDataType;
+      } else {
+        return getReturnType(opBinding, true);
+      }
+    }
+
+    private RelDataType getReturnType(SqlOperatorBinding opBinding, boolean convertVarCharToVar16Char) {
+      final int argLength = opBinding.getOperandCount();
+      TypeProtos.MajorType[] argTypes = new TypeProtos.MajorType[argLength];
+      ObjectInspector[] argOIs = new ObjectInspector[argLength];
+      for (int i = 0; i < argLength; ++i) {
+        try {
+          final RelDataType type = opBinding.getOperandType(i);
+          final TypeProtos.MinorType minorType = TypeInferenceUtils.getDrillTypeFromCalciteType(type);
+
+          TypeProtos.MajorType.Builder majorTypeBuilder = TypeProtos.MajorType.newBuilder().setMinorType(minorType);
+          if(type.isNullable()) {
+            majorTypeBuilder = majorTypeBuilder.setMode(TypeProtos.DataMode.OPTIONAL);
+          } else {
+            majorTypeBuilder = majorTypeBuilder.setMode(TypeProtos.DataMode.REQUIRED);
+          }
+
+          argTypes[i] = majorTypeBuilder.build();
+          if (convertVarCharToVar16Char && argTypes[i].getMinorType() == TypeProtos.MinorType.VARCHAR) {
+            argTypes[i] = Types.withMode(TypeProtos.MinorType.VAR16CHAR, argTypes[i].getMode());
+          }
+          argOIs[i] = ObjectInspectorHelper.getDrillObjectInspector(argTypes[i].getMode(), argTypes[i].getMinorType());
+        } catch(Exception e) {
+          throw getFunctionNotfoundException(opBinding);
+        }
+      }
+
+      // search in GenericUDF list
+      if(gUDFs != null) {
+        for (Class<? extends GenericUDF> clazz: gUDFs) {
+          final HiveFuncHolder holder = matchAndCreateGenericUDFHolder(clazz,
+              argTypes,
+              argOIs);
+          if (holder != null) {
+            return TypeInferenceUtils.createCalciteTypeWithNullability(
+                opBinding.getTypeFactory(),
+                TypeInferenceUtils.getCalciteTypeFromDrillType(holder.getReturnType().getMinorType()),
+                holder.getReturnType().getMode() != TypeProtos.DataMode.REQUIRED);
+          }
+        }
+      }
+
+      // search in UDF list
+      if(uDFs != null) {
+        for (Class<? extends UDF> clazz : uDFs) {
+          final HiveFuncHolder holder = matchAndCreateUDFHolder(opBinding.getOperator().getName().toLowerCase(),
+              clazz,
+              argTypes,
+              argOIs);
+          if (holder != null) {
+            return TypeInferenceUtils.createCalciteTypeWithNullability(
+                opBinding.getTypeFactory(),
+                TypeInferenceUtils.getCalciteTypeFromDrillType(holder.getReturnType().getMinorType()),
+                holder.getReturnType().getMode() != TypeProtos.DataMode.REQUIRED);
+          }
+        }
+      }
+
+
+      throw getFunctionNotfoundException(opBinding);
+    }
+
+    private UserException getFunctionNotfoundException(SqlOperatorBinding opBinding) {
+      String operandTypes = "";
+      for(int j = 0; j < opBinding.getOperandCount(); ++j) {
+        operandTypes += opBinding.getOperandType(j).getSqlTypeName();
+        if(j < opBinding.getOperandCount() - 1) {
+          operandTypes += ",";
+        }
+      }
+
+      return UserException
+          .functionError()
+          .message(String.format("%s does not support operand types (%s)",
+              opBinding.getOperator().getName(),
+              operandTypes))
+          .build(logger);
+    }
+  }
 }
